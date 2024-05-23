642dad2e6a89869c60c3d279237ba54045724429
cristhianllanque <cristhianllanque65@gmail.com>
2024-05-23 17:19:17 -0500

    Update home_drawer.dart
    Minombre

--- lib/drawer/home_drawer.dart
+++ lib/drawer/home_drawer.dart
@@ -113,7 +113,7 @@ class _HomeDrawerState extends State<HomeDrawer> {
                   Padding(
                     padding: const EdgeInsets.only(top: 8, left: 4),
                     child: Text(
-                      (name==null?"Anonimo":name)! + (" ") + (email==null?"Anomim":email)!,
+                      (name==null?"Cristhian Edy Llanque Tipo":name)! + (" ") + (email==null?"Anomim":email)!,
                       textAlign: TextAlign.center,
                       style: TextStyle(
                         fontWeight: FontWeight.w600,
